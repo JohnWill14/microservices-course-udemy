96d4130baf36634f5f6b93e299ca132168cc889e
John William <johnwill.v.2017@gmail.com>
2022-12-03 20:03:26 -0300

    :technologist: add structure initial spring project

---
+++ rest-full-web-service/src/test/java/br/com/william/restfullwebservice/RestFullWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.william.restfullwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
